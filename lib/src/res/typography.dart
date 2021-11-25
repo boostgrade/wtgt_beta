ddc044e1df6445f00365a57e43bd00e9e2b526d6
Maxim Movchan <maksmovchan@icloud.com>
2021-11-25 21:38:22 +0300

    'font imported'

---
+++ lib/src/res/typography.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppTypography{
+
+  static const String fontFamily='Montserrat';
+
+  TextStyle normal(double fontSize)=>TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: fontSize,
+    color: const Color(0x00000000));
+
+  TextStyle SemiBold(double fontSize,FontWeight fontWeight)=>TextStyle(
+      fontFamily: fontFamily,
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: const Color(0x00000000));
+
+}
