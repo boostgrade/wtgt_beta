e1a92985a5bf65a4b222814c8bd554e324e9c32d
Constantine <fateevke@gmail.com>
2021-11-29 23:00:15 +0300

    Exseptions try

--- lib/src/services/exceptions/server/server_error_mapper.dart
+++ lib/src/services/exceptions/server/server_error_mapper.dart
@@ -6,16 +6,27 @@ class ServerErrorMapper {
   /// Для обработки остальных серверных ошибок
   /// необходимо задать новые целочисленные константы, соотвтетсвующие кодам.
   static const int _notFound = 404;
+  static const int _unauthorized = 401;
+  static const int _badRequest = 400;
 
-  static Exception fromStatusCode(int statusCode) {
+  static Exception fromStatusCode(int statusCode, String statusDescription) {
     /// Для обработки остальных серверных ошибок
     /// нужно написать дополнительные блоки в условном выражении,
     /// предварительно создав классы исключений.
     if (statusCode == _notFound) {
-      return NotFoundException();
+      return NotFoundException(statusCode, statusDescription);
+    }
+
+    if (statusCode == _unauthorized) {
+      // Необходимо перезапросить токен
+      return UnauthorizedException(statusCode, statusDescription);
+    }
+
+    if (statusCode == _badRequest) {
+      return BadRequestException(statusCode, statusDescription);
     }
 
     /// Возвращается по-умолчанию для остальных ошибок
-    return ServerErrorException();
+    return ServerErrorException(statusCode, statusDescription);
   }
 }
