30a53daa7db020cd02d3e0229f4b358725c5697e
Maxim Movchan <maksmovchan@icloud.com>
2021-11-30 15:34:52 +0300

    'created BaseButton'

--- lib/src/ui/uikit/base_button.dart
+++ lib/src/ui/uikit/base_button.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:where_to_go_today/src/res/typography.dart';
+import 'package:where_to_go_today/src/ui/res/colors/colors.dart';
 
-class BaseButton extends StatelessWidget{
+enum Status { active, inavctive, loading }
+class BaseButton extends StatefulWidget{
+
+  final String label;
+  final void Function() onPressed;
+  Status status;
+
+  BaseButton(
+      {Key? key,required this.label, required this.onPressed, required this.status,}) : super(key : key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BaseButtonState();
+  }
+
+}
+class _BaseButtonState extends State<BaseButton> {
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Opacity(
+      opacity: widget.status == Status.inavctive ? 0.4 : 1.0,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+            const RoundedRectangleBorder(
+              side: BorderSide(
+                color: ProjectColors.buttonStrokeBlue,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+          ),
+          backgroundColor:
+              MaterialStateProperty.all(ProjectColors.backgroundYellow),
+        ),
+        onPressed: widget.status == Status.inavctive
+            ? ()=>{}
+            : onTap(widget.onPressed),
+        child: Text(
+          widget.label,
+          style: AppTypography().semiBold(16, FontWeight.w500),
+        ),
+      ),
+    );
   }
-  
+  onTap(void Function() onPressed){
+    setState((){widget.status=Status.loading;});
+    onPressed;
+    setState(() {widget.status = Status.active;});
+  }
 }
