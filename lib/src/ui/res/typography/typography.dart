cfd146f4d4826244c7818839a44857c1c6130bbf
Maxim Movchan <maksmovchan@icloud.com>
2021-11-30 17:07:05 +0300

    'edits typography'

--- lib/src/ui/res/typography/typography.dart
+++ lib/src/ui/res/typography/typography.dart
@@ -1,3 +1,57 @@
+import 'dart:ui';
+
 abstract class AppTypography {
-  
+
+  static const String fontFamily='Montserrat';
+
+  final TextStyle light12 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 12.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle light16 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle normal16 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 16.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle normal18 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 18.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle semiBold12 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 12.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle semiBold16 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 16.0,
+    color: const Color(0x00000000),);
+
+  final TextStyle semiBold24 = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w600,
+    fontSize: 24.0,
+    color: const Color(0x00000000),);
+
+
+
+  TextStyle semiBold(double fontSize,FontWeight fontWeight)=>TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+    color: const Color(0x00000000),);
+
 }
--- lib/src/ui/uikit/base_button.dart
+++ lib/src/ui/uikit/base_button.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:where_to_go_today/src/res/typography.dart';
 import 'package:where_to_go_today/src/ui/res/colors/colors.dart';
+import 'package:where_to_go_today/src/ui/res/typography/typography.dart';
 
 enum Status { active, inavctive, loading }
 class BaseButton extends StatefulWidget{
 
   final String label;
   final void Function() onPressed;
-  Status status;
 
-  BaseButton(
-      {Key? key,required this.label, required this.onPressed, required this.status,}) : super(key : key);
+  const BaseButton(
+      {Key? key,required this.label, required this.onPressed,}) : super(key : key);
 
   @override
   State<StatefulWidget> createState() {
@@ -19,7 +19,7 @@ class BaseButton extends StatefulWidget{
 
 }
 class _BaseButtonState extends State<BaseButton> {
-
+  final Status status=Status.active;
   @override
   Widget build(BuildContext context) {
     return Opacity(
@@ -43,7 +43,7 @@ class _BaseButtonState extends State<BaseButton> {
             : onTap(widget.onPressed),
         child: Text(
           widget.label,
-          style: AppTypography().semiBold(16, FontWeight.w500),
+          style: AppTypography.normal16,
         ),
       ),
     );
