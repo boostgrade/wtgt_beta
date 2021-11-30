858fa75c2b5cf633f8be4b07a482c48e26de2886
Maxim Movchan <maksmovchan@icloud.com>
2021-11-30 14:37:41 +0300

    'created BaseButton class'

---
+++ lib/src/ui/uikit/base_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BaseButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
