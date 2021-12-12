c215dcd92f7303056f51301192ce7509e10bd453
Maxim Movchan <maksmovchan@icloud.com>
2021-12-12 19:13:21 +0300

    'feature:added retrofit'

--- lib/src/modules/authapi/models/response/user_model.dart
+++ lib/src/modules/authapi/models/response/user_model.dart
@@ -1,10 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
-
 part 'user_model.g.dart';
 
 /// Модель пользователя, прилтеающая после запроса авторизации
 @JsonSerializable()
-class User{
+class UserResponse {
   int id;
   String name;
   String lastName;
@@ -13,17 +12,17 @@ class User{
   String token;
   String refreshToken;
 
-  User({
+  UserResponse({
     required this.id,
     required this.name,
     required this.lastName,
     required this.phone,
-  required this.birthDate,
-  required this.token,
-  required this.refreshToken
+    required this.birthDate,
+    required this.token,
+    required this.refreshToken,
   });
 
-  factory User.fromJson(Map<String,dynamic> json) => _$UserFromJson(json);
+  factory UserResponse.fromJson(Map<String, dynamic> json) => _$UserResponseFromJson(json);
 
-  Map<String,dynamic> toJson() => _$UserToJson(this);
+  Map<String, dynamic> toJson() => _$UserResponseToJson(this);
 }
