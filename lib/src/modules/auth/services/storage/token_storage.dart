77d368fb26348a535856ca474c7d041b1f7eeff8
Constantine <fateevke@gmail.com>
2021-12-15 00:40:24 +0300

    хайв добавлен в мейн, добавлена документация к методам, добавлена приватность

--- lib/src/modules/auth/services/storage/token_storage.dart
+++ lib/src/modules/auth/services/storage/token_storage.dart
@@ -2,31 +2,33 @@ import 'dart:io';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-class AppStorage {
-  AppStorage() {
-    Hive.initFlutter();
+/// Класс необходим для сохранения токена авторизации
+
+class AuthTokenStorage {
+  /// Метод создающий хранилище
+  void _createStorage(String boxName) async {
+    var _box = await Hive.openBox<dynamic>(boxName);
+    _box.close();
   }
 
-  void createBox(boxName) async {
-    var box = await Hive.openBox<dynamic>(boxName);
-    box.close();
+  /// Метод помещающий данные в хранилище
+  void _saveToken(String boxName, String boxKey, dynamic boxData) async {
+    var _box = await Hive.openBox<dynamic>(boxName);
+    await _box.put(boxKey, boxData);
+    _box.close();
   }
 
-  void putToBox(boxName, boxKey, boxData) async {
-    var box = await Hive.openBox<dynamic>(boxName);
-    await box.put(boxKey, boxData);
-    box.close();
+  /// Метод добавляющий данные в хранилище
+  void _addToStorage(String boxName, dynamic boxData) async {
+    var _box = await Hive.openBox<dynamic>(boxName);
+    await _box.add(boxData);
+    _box.close();
   }
 
-  void addToBox(boxName, boxData) async {
-    var box = await Hive.openBox<dynamic>(boxName);
-    await box.add(boxData);
-    box.close();
-  }
-
-  void getFromBox(boxName, boxData) async {
-    var box = await Hive.openBox<dynamic>(boxName);
-    box.get(boxData);
-    box.close();
+  /// Метод с помощью которого можно получить данные(токен) из хранилища
+  void _getToken(String boxName, dynamic boxData) async {
+    var _box = await Hive.openBox<dynamic>(boxName);
+    _box.get(boxData);
+    _box.close();
   }
 }
