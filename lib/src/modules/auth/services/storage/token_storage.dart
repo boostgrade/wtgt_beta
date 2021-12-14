36aa87b7478f4849d44fdf7caeb2fabff8ef4c85
Constantine <fateevke@gmail.com>
2021-12-14 18:28:24 +0300

    added flutter init

--- lib/src/modules/auth/services/storage/token_storage.dart
+++ lib/src/modules/auth/services/storage/token_storage.dart
@@ -2,6 +2,31 @@ import 'dart:io';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void appInitHive() async {
-  await Hive.initFlutter();
+class AppStorage {
+  AppStorage() {
+    Hive.initFlutter();
+  }
+
+  void createBox(boxName) async {
+    var box = await Hive.openBox<dynamic>(boxName);
+    box.close();
+  }
+
+  void putToBox(boxName, boxKey, boxData) async {
+    var box = await Hive.openBox<dynamic>(boxName);
+    await box.put(boxKey, boxData);
+    box.close();
+  }
+
+  void addToBox(boxName, boxData) async {
+    var box = await Hive.openBox<dynamic>(boxName);
+    await box.add(boxData);
+    box.close();
+  }
+
+  void getFromBox(boxName, boxData) async {
+    var box = await Hive.openBox<dynamic>(boxName);
+    box.get(boxData);
+    box.close();
+  }
 }
