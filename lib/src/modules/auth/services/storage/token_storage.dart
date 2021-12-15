1f0727baec40f565f7b535e0a461c04e5edbb1c1
Constantine <fateevke@gmail.com>
2021-12-15 23:01:02 +0300

    добавлены константы, изменены методы

--- lib/src/modules/auth/services/storage/token_storage.dart
+++ lib/src/modules/auth/services/storage/token_storage.dart
@@ -3,32 +3,35 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 /// Класс необходим для сохранения токена авторизации
-
 class AuthTokenStorage {
+  static const String boxName = 'tokenStorage';
+  static const String boxKey = 'token';
+
   /// Метод создающий хранилище
-  void _createStorage(String boxName) async {
+  Future initStorage() async {
     var _box = await Hive.openBox<dynamic>(boxName);
-    _box.close();
+    return _box;
   }
 
   /// Метод помещающий данные в хранилище
-  void _saveToken(String boxName, String boxKey, dynamic boxData) async {
-    var _box = await Hive.openBox<dynamic>(boxName);
-    await _box.put(boxKey, boxData);
+  void saveToken(String token) async {
+    var _box = await initStorage();
+    await _box.put(boxKey, token);
     _box.close();
   }
 
   /// Метод добавляющий данные в хранилище
-  void _addToStorage(String boxName, dynamic boxData) async {
-    var _box = await Hive.openBox<dynamic>(boxName);
+  void addToStorage(dynamic boxData) async {
+    var _box = await initStorage();
     await _box.add(boxData);
     _box.close();
   }
 
   /// Метод с помощью которого можно получить данные(токен) из хранилища
-  void _getToken(String boxName, dynamic boxData) async {
-    var _box = await Hive.openBox<dynamic>(boxName);
-    _box.get(boxData);
-    _box.close();
+  Future getToken(dynamic boxData) async {
+    var _box = await initStorage();
+    var token = _box.get(boxData);
+
+    return token;
   }
 }
