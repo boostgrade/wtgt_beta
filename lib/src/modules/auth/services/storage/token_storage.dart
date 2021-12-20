d8ee221009561077aea628e84dac16262f9e9a52
Constantine <fateevke@gmail.com>
2021-12-20 13:09:15 +0300

    убран хайв

--- lib/src/modules/auth/services/storage/token_storage.dart
+++ lib/src/modules/auth/services/storage/token_storage.dart
@@ -1,35 +1,6 @@
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 /// Класс необходим для сохранения токена авторизации
-class AuthTokenStorage {
-  static const String _boxName = 'tokenStorage';
-  static const String _boxKey = 'token';
-
-  /// Метод создающий хранилище
-  Future<Box<String>> initStorage() async {
-    var _box = await Hive.openBox<String>(_boxName);
-
-    return _box;
-  }
-
-  /// Метод помещающий данные в хранилище
-  void saveToken(String token) async {
-    var _box = await initStorage();
-    await _box.put(_boxKey, token);
-    _box.close();
-  }
-
-  /// Метод с помощью которого можно получить данные(токен) из хранилища
-  Future<String?> getToken() async {
-    var _box = await initStorage();
-    var token = _box.get(_boxKey);
-
-    return token;
-  }
-}
-
 class AuthSecureTokenStorage {
   final tokenStorage = const FlutterSecureStorage();
   static const String _storageName = 'tokenStorage';
