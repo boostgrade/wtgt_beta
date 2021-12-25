a762ee08c0725ea245258d920f3183dc5de24f20
Maxim Movchan <58935940+GhostuSs@users.noreply.github.com>
2021-12-25 12:12:39 +0300

    Add files via upload

---
+++ lib/src/modules/auth/models/response/register_response_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RegisterResponse _$RegisterResponseFromJson(Map<String, dynamic> json) =>
+    RegisterResponse(
+      json['description'] as String,
+    );
+
+Map<String, dynamic> _$RegisterResponseToJson(RegisterResponse instance) =>
+    <String, dynamic>{
+      'description': instance.description,
+    };
