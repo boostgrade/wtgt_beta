05186a2ab6a29f899586ddcc2aa1ed8154943c26
Constantine <fateevke@gmail.com>
2021-12-26 16:35:17 +0300

    логика по вводу номера

---
+++ lib/src/modules/auth/ui/sign_in/sign_in_vm.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:where_to_go_today/src/ui/base/view_model.dart';
+import 'package:where_to_go_today/src/ui/errors_handling/error_handler.dart';
+
+class SignIn extends ViewModel {
+  SignIn(ErrorHandler errorHandler) : super(errorHandler);
+}
