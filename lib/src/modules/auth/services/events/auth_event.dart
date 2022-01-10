4ae184aa3f3c918aa9c80bae91a680428b024a88
Mirko <z.naumovich@spb.mcart.ru>
2022-01-10 23:42:58 +0300

    Мелкие правки

--- lib/src/modules/auth/services/events/auth_event.dart
+++ lib/src/modules/auth/services/events/auth_event.dart
@@ -3,17 +3,11 @@ abstract class AuthEvent {}
 class SmsAuthEvent extends AuthEvent {
   final String code;
 
-  @override
-  List<Object?> get fields => [code];
-
   SmsAuthEvent(this.code);
 }
 
 class ExternalAuthEvent extends AuthEvent {
   final Enum type;
-
-  @override
-  List<Object?> get props => [type];
 
   ExternalAuthEvent(this.type);
 }
@@ -21,16 +15,10 @@ class ExternalAuthEvent extends AuthEvent {
 class PhoneAuthEvent extends AuthEvent {
   final String phone;
 
-  @override
-  List<Object?> get props => [phone];
-
   PhoneAuthEvent(this.phone);
 }
 
-class LogoutEvent extends AuthEvent {
-  @override
-  List<Object?> get props => [];
-}
+class LogoutEvent extends AuthEvent {}
 
 class RegistrationEvent extends AuthEvent {
 
@@ -38,9 +26,6 @@ class RegistrationEvent extends AuthEvent {
   final String lastName;
   final String phone;
   final DateTime birthDate;
-
-  @override
-  List<Object?> get props => [name, lastName, phone, birthDate];
 
   RegistrationEvent(this.name, this.lastName, this.phone, this.birthDate);
 }
