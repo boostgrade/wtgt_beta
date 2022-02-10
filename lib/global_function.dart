823a4a1a98cec989acacdcff17ee3e17f406a677
Nikolay <n.ivanov@spb.mcart.ru>
2022-02-10 21:53:31 +0300

    Исправление замечаний

--- lib/global_function.dart
+++ lib/global_function.dart
@@ -2,7 +2,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:where_to_go_today/src/modules/auth/storage/token_storage.dart';
 
 /// Инициализация хранилища Hive
-void iniHive() async {
+Future<void> iniHive() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TokenStorageAdapter());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   final globalDeps = AppDependencies();
   await globalDeps.init();
 
-  iniHive();
+  await iniHive();
 
   runApp(
     Provider(
