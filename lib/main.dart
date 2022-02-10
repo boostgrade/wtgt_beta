9f472f609174cdbe07bef5931759cdc04a73a6af
Nikolay <n.ivanov@spb.mcart.ru>
2022-02-10 09:55:06 +0300

    Сторедж для токена и рефреш токена

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:where_to_go_today/src/app/ui/app_store.dart';
 import 'package:where_to_go_today/src/di/app_dependency.dart';
+import 'package:where_to_go_today/src/modules/auth/storage/token_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,6 +10,9 @@ import 'src/app/ui/app.dart';
 void main() async {
   final globalDeps = AppDependencies();
   await globalDeps.init();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(TokenStorageAdapter());
 
   runApp(
     Provider(
