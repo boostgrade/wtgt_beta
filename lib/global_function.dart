68d885249312d987d6541c7de4afa5516ae55c6a
Nikolay <n.ivanov@spb.mcart.ru>
2022-02-10 11:21:40 +0300

    Добавил глобальную функцию initHive

---
+++ lib/global_function.dart
@@ -0,0 +1,8 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:where_to_go_today/src/modules/auth/storage/token_storage.dart';
+
+/// Инициализация хранилища Hive
+void iniHive() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TokenStorageAdapter());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:where_to_go_today/global_function.dart';
 import 'package:where_to_go_today/src/app/ui/app_store.dart';
 import 'package:where_to_go_today/src/di/app_dependency.dart';
-import 'package:where_to_go_today/src/modules/auth/storage/token_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,8 +10,7 @@ void main() async {
   final globalDeps = AppDependencies();
   await globalDeps.init();
 
-  await Hive.initFlutter();
-  Hive.registerAdapter(TokenStorageAdapter());
+  iniHive();
 
   runApp(
     Provider(
