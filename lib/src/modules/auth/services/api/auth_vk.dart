c153a0573563823d696010598c99722e6e49df64
Constantine <fateevke@gmail.com>
2022-02-15 11:25:09 +0300

    auth vk

--- lib/src/modules/auth/services/api/auth_vk.dart
+++ lib/src/modules/auth/services/api/auth_vk.dart
@@ -1,25 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_login_vk/flutter_login_vk.dart';
+import 'package:mobx/mobx.dart';
 
-class AuthVK {
-  final vk = VKLogin();
+part 'auth_vk.g.dart';
+
+enum ScreenState {
+  content,
+  loading,
+  error,
+}
+
+class AuthVK = _AuthVK with _$AuthVK;
+
+abstract class _AuthVK with Store {
+  @observable
+  ScreenState state = ScreenState.content;
 
   VKAccessToken? _vkToken;
   VKUserProfile? _vkProfile;
   String? _vkEmail;
   bool _vkSdkInitialized = false;
 
-  Future<void> initVK() async {
-    await vk.initSdk();
-    _vkSdkInitialized = true;
-  }
+  @action
+  Future<void> loginVK() async {
+    final vk = VKLogin();
 
-  Future<void> onPressVk() async {
-    final result = await vk.logIn(scope: [
+    await vk.initSdk(scope: [
       VKScope.email,
     ]);
+
+    final loginResult = await vk.logIn();
+
+    if (loginResult.isValue) {
+      final result = loginResult.asValue!.value;
+
+      if (!result.isCanceled) {
+        _handleLogin(result);
+      }
+    }
   }
 
-  Future<void> vkLogOut() async {
-    await vk.logOut();
+  Future<void> _handleLogin(VKLoginResult result) async {
+    // ignore: avoid_print
+    print('${result.accessToken}');
   }
 }
